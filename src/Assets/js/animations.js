ae182a6de54f304d803bc245e3a024bc2ea869d3
CarlaCGDM <ncardillo@iesrafaelalberti.es>
2022-12-10 01:34:59 +0100

    css de pantalla de login, algunos componentes

---
+++ src/Assets/js/animations.js
@@ -0,0 +1,19 @@
+/* DETECTAR EL CAMBIO DE TAMAÑO DE LA PANTALLA PARA VOLVER A LA VISTA DEFAULT*/
+
+/*EXPANDIR MENU BUSQUEDA MOVIL*/
+
+/*COLAPSAR MENU BUSQUEDA MOVIL*/
+
+/*--Mostrar/ocultar menú lateral de navegación--*/
+
+function toggleNavbar() {
+    let nav = document.querySelector("nav");
+    if (nav.style.right == '0vw') {
+        nav.style.right = '-20vw';
+        nav.style.opacity = '90%';
+    } else {
+        nav.style.right = '0vw';
+        
+        nav.style.opacity = '100%';
+    }
+}
