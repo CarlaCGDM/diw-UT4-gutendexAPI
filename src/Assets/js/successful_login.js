247b3c434e28461f54dc77552a5949bee18e5e71
CarlaCGDM <ncardillo@iesrafaelalberti.es>
2022-12-11 23:24:22 +0100

    compilado final

--- src/Assets/js/successful_login.js
+++
@@ -1,5 +0,0 @@
-//aqui se muestran u ocultan todos los elementos que deben cambiar en la version movil
-//cuyo cambio no era posible hacer mediante media queries
-
-//cambio del avatar a version movil
-//el avatar movil tiene el metodo onclick que muestra el menu de navegacion movil
